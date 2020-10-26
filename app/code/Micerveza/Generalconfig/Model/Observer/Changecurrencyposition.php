e33ed7ad62fe7c3c90459a39946d0776858a4de3
mairetgm <mairetgm@gmail.com>
2020-10-26 08:30:18 -0500

    CSS and module for change currency position

---
+++ app/code/Micerveza/Generalconfig/Model/Observer/Changecurrencyposition.php
@@ -0,0 +1,12 @@
+<?php        
+namespace Micerveza\Generalconfig\Model\Observer;
+use Magento\Framework\Event\ObserverInterface;    
+class Changecurrencyposition implements ObserverInterface
+{
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {    
+        $currencyOptions = $observer->getEvent()->getCurrencyOptions();    
+        $currencyOptions->setData('position', \Magento\Framework\Currency::LEFT);
+        return $this;
+    }    
+}
