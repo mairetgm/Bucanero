e682a9c98b891a4542795872be85d153a0076f6b
mairetgm <mairetgm@gmail.com>
2020-10-26 08:31:53 -0500

    config to activate module

--- app/etc/config.php
+++ app/etc/config.php
@@ -265,6 +265,7 @@ return [
         'Mageplaza_Core' => 1,
         'Mageplaza_Smtp' => 1,
         'Mageplaza_SocialLogin' => 1,
+        'Micerveza_Generalconfig' => 1,
         'Rootways_Psigate' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
